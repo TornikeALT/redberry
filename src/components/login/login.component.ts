0a6077aa01102cb7d9d516d37c97cc7164d06a87
TornikeALT <tornikealtunashvili@yahoo.com>
2025-09-24 16:03:43 +0400

    update authservice
    also add erromsg

--- src/components/login/login.component.ts
+++ src/components/login/login.component.ts
@@ -1,18 +1,37 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { RouterLink } from '@angular/router';
+import { AuthService } from '../../services/auth.service';
+import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-login',
-  imports: [RouterLink, FormsModule],
+  imports: [RouterLink, FormsModule, CommonModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css',
 })
 export class LoginComponent {
   email = '';
   password = '';
+  errorMessage = '';
+
+  constructor(private authService: AuthService, private router: Router) {}
 
   onSubmit() {
-    console.log(this.email, this.password);
+    const credentials = {
+      email: this.email,
+      password: this.password,
+    };
+
+    this.authService.login(credentials).subscribe({
+      next: (response) => {
+        localStorage.setItem('token', response.token);
+        this.router.navigate(['/products']);
+      },
+      error: (err) => {
+        this.errorMessage = 'Invalid email or password';
+      },
+    });
   }
 }
--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -1,16 +1,34 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { BehaviorSubject, Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
   private loginUrl = 'https://api.redseam.redberryinternship.ge/api/login';
+  private loggedIn$ = new BehaviorSubject<boolean>(this.hasToken());
 
   constructor(private http: HttpClient) {}
 
   login(credentials: { email: string; password: string }): Observable<any> {
     return this.http.post(this.loginUrl, credentials);
+  }
+
+  setToken(token: string) {
+    localStorage.setItem('token', token);
+    this.loggedIn$.next(true);
+  }
+
+  logOut() {
+    localStorage.removeItem('token');
+    this.loggedIn$.next(false);
+  }
+
+  isLoggedIn(): Observable<boolean> {
+    return this.loggedIn$.asObservable();
+  }
+  private hasToken(): boolean {
+    return !!localStorage.getItem('token');
   }
 }
