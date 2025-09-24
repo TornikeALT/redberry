107b81a4f79c4c392c25d915120dac98b477ecfe
TornikeALT <tornikealtunashvili@yahoo.com>
2025-09-24 12:09:51 +0400

    add authentification service

--- src/components/login/login.component.ts
+++ src/components/login/login.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-login',
-  imports: [RouterLink],
+  imports: [RouterLink, FormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css',
 })
-export class LoginComponent {}
+export class LoginComponent {
+  email = '';
+  password = '';
+
+  onSubmit() {
+    console.log(this.email, this.password);
+  }
+}
