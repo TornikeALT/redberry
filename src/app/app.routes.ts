a3d4b16c2b947287d981368c196fcb2a8d3689da
TornikeALT <tornikealtunashvili@yahoo.com>
2025-09-24 18:11:47 +0400

    add login guard
    no access to user to login and register pages when user is already logged in

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,11 +3,12 @@ import { ProductsComponent } from '../components/products/products.component';
 import { LoginComponent } from '../components/login/login.component';
 import { RegisterComponent } from '../components/register/register.component';
 import { ProductDetailComponent } from '../components/product-detail/product-detail.component';
+import { LoginGuard } from '../guard/login.guard';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'products', pathMatch: 'full' },
   { path: 'products', component: ProductsComponent },
   { path: 'products/:id', component: ProductDetailComponent },
-  { path: 'login', component: LoginComponent },
-  { path: 'register', component: RegisterComponent },
+  { path: 'login', component: LoginComponent, canActivate: [LoginGuard] },
+  { path: 'register', component: RegisterComponent, canActivate: [LoginGuard] },
 ];
